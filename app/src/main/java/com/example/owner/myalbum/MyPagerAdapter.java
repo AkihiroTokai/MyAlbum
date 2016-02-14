3b5103509db57ff845e2b59f453dc97649d84d4e
ToukaiAkihiro <tokaimas@yahoo.co.jp>
2016-02-14 14:34:45 +0900

    Refactoring

--- app/src/main/java/com/example/owner/myalbum/MyPagerAdapter.java
+++ app/src/main/java/com/example/owner/myalbum/MyPagerAdapter.java
@@ -1,7 +1,6 @@
 package com.example.owner.myalbum;
 
 
-
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
@@ -10,24 +9,25 @@ import android.support.v13.app.FragmentPagerAdapter;
 /**
  * Created by OWNER on 2015/05/09.
  */
-public class MyPagerAdapter extends FragmentPagerAdapter{
+public class MyPagerAdapter extends FragmentPagerAdapter {
     private int[] mImageList;
+
     public MyPagerAdapter(FragmentManager fm, int[] imageList) {
         super(fm);
         mImageList = imageList;
     }
 
     @Override
-    public Fragment getItem(int position ) {
+    public Fragment getItem(int position) {
         PictureFragment fragment = new PictureFragment();
         Bundle args = new Bundle();
-        args.putInt("image_id",mImageList[position]);
+        args.putInt("image_id", mImageList[position]);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public int getCount(){
+    public int getCount() {
         return mImageList.length;
     }
 }
